cc71e7c8b8ff0ed80a78c84fdeeeb0e123293b82
timothee <tim.dorand@gmail.com>
2016-05-14 10:39:37 +0200

    Appareil photo pour upload image

--- app/src/main/java/com/shuffle/theo/shuffletrip/UserActivity.java
+++ app/src/main/java/com/shuffle/theo/shuffletrip/UserActivity.java
@@ -1,15 +1,18 @@
 package com.shuffle.theo.shuffletrip;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,21 +22,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
+
 
 public class UserActivity extends AppCompatActivity implements OnClickListener {
     public ImageButton home;
@@ -48,6 +48,15 @@ public class UserActivity extends AppCompatActivity implements OnClickListener {
     ImageView imageToUpload, downloadedImage;
     Button bUploadImage, bDownloadImage;
     EditText uploadImageName, downloadImageName;
+
+    Button btpic, btnup;
+    private Uri fileUri;
+    String picturePath;
+    Uri selectedImage;
+    Bitmap photo;
+    String ba1;
+    public static String URL = "http://timothee-dorand.fr/shuffletrip";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +75,29 @@ public class UserActivity extends AppCompatActivity implements OnClickListener {
         String pseudo = intent.getStringExtra("pseudo");
         pseudo2.setText("Bonjour " + pseudo);
 
+
         /*Code Tim*/
-        super.onCreate(savedInstanceState);
+
+        btpic = (Button) findViewById(R.id.cpic);
+        btpic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickpic();
+            }
+        });
+
+        btnup = (Button) findViewById(R.id.up);
+        btnup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upload();
+            }
+        });
+
+
+        /*OLD UPLOAD*/
+
+        /*super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
@@ -83,7 +113,26 @@ public class UserActivity extends AppCompatActivity implements OnClickListener {
         imageToUpload.setOnClickListener(this);
 //        bSelectImage.setOnClickListener(this);
         bUploadImage.setOnClickListener(this);
-        bDownloadImage.setOnClickListener(this);
+        bDownloadImage.setOnClickListener(this);*/
+    }
+
+    private void upload() {
+        // Image location URL
+        Log.e("path", "----------------" + picturePath);
+
+        // Image
+        Bitmap bm = BitmapFactory.decodeFile(picturePath);
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+        byte[] ba = bao.toByteArray();
+        int flag = 0; // On passe par défaut 0 = Base64.DEFAULT
+        ba1 = Base64.encodeToString(ba, flag);
+
+        Log.e("base64", "-----" + ba1);
+
+        // Upload image to server
+        new uploadToServer().execute();
+
     }
 
     @Override
@@ -99,22 +148,105 @@ public class UserActivity extends AppCompatActivity implements OnClickListener {
             this.startActivity(I_News);
         }
 
-            switch (v.getId()) {
+            /*switch (v.getId()) {
                 case R.id.imageToUpload:
                     Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                     startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
                     break;
                 case R.id.bUploadImage:
                     Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
-                    new UploadImage(image, uploadImageName.getText().toString()).execute();
+                    new UploadImagew(image, uploadImageName.getText().toString()).execute();
                     break;
                 case R.id.bDownloadImage:
 
                     break;
 
-            }
+            }*/
 
         }
+
+    /* TEST NEW UPLOAD */
+
+
+    private void clickpic() {
+        // Check Camera
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // Open default camera
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+            // start the image capture Intent
+            startActivityForResult(intent, 100);
+
+        } else {
+            Toast.makeText(getApplication(), "Camera not supported", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+
+            selectedImage = data.getData();
+            photo = (Bitmap) data.getExtras().get("data");
+
+            // Cursor to get image uri to display
+
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ImageView imageView = (ImageView) findViewById(R.id.Imageprev);
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+    public class uploadToServer extends AsyncTask<Void, Void, String> {
+
+        private ProgressDialog pd = new ProgressDialog(UserActivity.this);
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setMessage("Wait image uploading!");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("base64", ba1));
+            nameValuePairs.add(new BasicNameValuePair("ImageName", System.currentTimeMillis() + ".jpg"));
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(URL);
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                String st = EntityUtils.toString(response.getEntity());
+                Log.v("log_tag", "In the try Loop" + st);
+
+            } catch (Exception e) {
+                Log.v("log_tag", "Error in http connection " + e.toString());
+            }
+            return "Success";
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            pd.hide();
+            pd.dismiss();
+        }
+    }
+}
+
+    /*OLD UPLOAD*/
+/*
 
         @Override
         protected void onActivityResult ( int requestCode, int resultCode, Intent data){
@@ -209,4 +341,8 @@ public class UserActivity extends AppCompatActivity implements OnClickListener {
         HttpConnectionParams.setSoTimeout(httpRequestParams, 1000 * 30);
         return httpRequestParams;
     }
-}
+*/
+
+
+
+
